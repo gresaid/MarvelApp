eb16c4f3dc20416c692d7c26f96092a07f05fc39
Vladimir <gresvladimir02@gmail.com>
2022-11-17 20:00:32 +0600

    1 Laba
    Осталось только разделить экран на треугольник и покрасить этот треугольник

--- Marvel/Marvel/ViewController.swift
+++ Marvel/Marvel/ViewController.swift
@@ -20,10 +20,10 @@ class ViewController: UIViewController {
     
   override func viewDidLoad() {
         super.viewDidLoad()
-        initialize()
         registerCollectionViewCells()
-        applyConstraints()
         
+        view.addSubview(collectionView)
+        initialize()
     }
     private func initialize(){
         view.backgroundColor = UIColor(red: 42/255, green: 39/255, blue: 43/255, alpha: 1.0)
@@ -33,29 +33,37 @@ class ViewController: UIViewController {
             
         
         let headerText = UILabel()
+        view.addSubview(headerText)
+        view.addSubview(logo)
         headerText.text = "⛰ Choose your hero"
         headerText.font = UIFont.boldSystemFont(ofSize: 35)
         headerText.textColor = .white
-        view.addSubview(headerText)
-        view.addSubview(logo)
+        collectionView.snp.makeConstraints { maker in
+            maker.left.equalTo(view.snp.left)
+            maker.right.equalTo(view.snp.right)
+            maker.top.equalTo(headerText.snp.bottom).offset(30)
+            maker.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-60)
+        }
         logo.snp.makeConstraints{maker in
                 maker.centerX.equalTo(view.snp.centerX)
                 maker.top.equalTo(view).offset(70.0)
                 maker.size.equalTo(CGSize(width: 146, height: 43))
         }
-
         headerText.snp.makeConstraints{maker in
             maker.left.equalToSuperview().inset(20)
             maker.top.equalToSuperview().inset(150)
         }
         
+        
     }
-      let cellWidth = (3 / 4) * UIScreen.main.bounds.width
-    let cellHeight = (2/4) * UIScreen.main.bounds.height
-      let sectionSpacing = (1 / 8) * UIScreen.main.bounds.width
-      let cellSpacing = (1 / 16) * UIScreen.main.bounds.width
+    
+    
       
-      let colors: [UIColor] = [.red, .green, .blue, .purple, .orange, .black, .cyan]
+    
+      let heroes = [heroesOptions(name: "Deadpool", colors: .red, image:UIImage(named: "deadpool")),
+                    heroesOptions(name: "Iron Man", colors: .yellow, image: UIImage(named: "Iron_man")),
+                    heroesOptions(name: "Spider man", colors: .blue, image: UIImage(named: "spider_man")),
+                    heroesOptions(name: "Thanos", colors: .purple, image: UIImage(named: "Thanos"))] //, .green, .blue, .purple, .orange, .black, .cyan]
       let cellId = "cell id"
       
       // MARK: - UI Components
@@ -63,14 +71,14 @@ class ViewController: UIViewController {
       lazy var collectionView: UICollectionView = {
           let layout = PagingCollectionViewLayout()
           layout.scrollDirection = .horizontal
-          layout.sectionInset = UIEdgeInsets(top: 0, left: sectionSpacing, bottom: 0, right: sectionSpacing)
-          layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
-          layout.minimumLineSpacing = cellSpacing
+          layout.sectionInset = UIEdgeInsets(top: 0, left: 60, bottom: 0, right: 60)
+          layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 120, height: 500)
+          layout.minimumLineSpacing = 40
           
           let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
           collectionView.translatesAutoresizingMaskIntoConstraints = false
           collectionView.showsHorizontalScrollIndicator = false
-          collectionView.backgroundColor = UIColor(red: 42/255, green: 39/255, blue: 43/255, alpha: 1.0)
+          collectionView.backgroundColor = .none
           collectionView.decelerationRate = .fast
           collectionView.dataSource = self
           return collectionView
@@ -81,32 +89,29 @@ class ViewController: UIViewController {
 
       
       private func registerCollectionViewCells() {
-          collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+          collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: cellId)
       }
       
-      private func applyConstraints() {
-          view.addSubview(collectionView)
-          collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-          collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-          collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-          collectionView.heightAnchor.constraint(equalToConstant: cellWidth).isActive = true
-      }
+      
   }
 
   // MARK: - CollectionView Data Source
-  extension ViewController: UICollectionViewDataSource {
-      
-      func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-          return colors.count
-      }
-      
-      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-          let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-          let color = colors[indexPath.item]
-          cell.backgroundColor = color
-          return cell
-      }
-
-
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return heroes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? CollectionViewCell else {
+            return .init()
+            // let h = heroes[indexPath.item]
+        }
+        cell.set(heroes_set: heroes[indexPath.item])
+            return cell
+        }
+    
+    
+   
 }
-
+    
